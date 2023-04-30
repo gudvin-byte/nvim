440c27647502606fef3ba0a67c4072d9e916e234
Gudvin <swmmci@gmail.com>
2023-04-30 16:43:26 +0200

    major update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,7 @@ vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', {noremap
 vim.api.nvim_set_keymap('n', '<Leader>fw', ':Telescope live_grep<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>fs', ':Telescope grep_string<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>fo', ':Telescope oldfiles<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>fp', ':Telescope git_files<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>u', ':UndotreeToggle<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>h', ':bprevious<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>l', ':bnext<CR>', {noremap = true, silent = true})
